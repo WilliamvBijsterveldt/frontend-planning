b32bb42f01ab5fb491a88775052e8dadbbd5f65c
William van Bijsterveldt <w.vanbijsterveldt@student.fontys.nl>
2022-11-23 10:51:46 +0100

    Added user CRUD functions

--- src/app/Components/users-list/users-list.component.ts
+++ src/app/Components/users-list/users-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../../Services/user.service";
+import {User} from "../../Models/user";
 
 @Component({
   selector: 'app-users-list',
@@ -7,8 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  constructor(private userService: UserService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  private getUsers() {
+    this.userService.getUser().subscribe(data => {
+      this.users = data;
+    });
+  }
+
+  updateUser(id: number) {
+    this.router.navigate(['update-user', id]);
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(data => {
+        console.log(data)
+        this.getUsers();
+      },
+      error => console.log(error));
   }
 }
